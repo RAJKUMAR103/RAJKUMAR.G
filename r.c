518ae5ca47a889fa5d7ec308c469f70c4138fda6
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-01-26 09:09:52 +0530

    Create r.c

---
+++ r.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("%d",enter the num);
+scanf("%d",&num);
+if(num<0)
+printf("%d",num is negative);
+else if(num>0)
+printf("%d",number is positive);
+else
+printf("%d",num is zero);
+}
