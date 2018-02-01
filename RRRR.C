6d7e53e57b5d5bd66719d1c5a9a2d860788c1de9
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-02-01 16:44:27 +0530

    Create RRRR.C

---
+++ RRRR.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("enter a word");
+scanf("%c",&a);
+if((a>=z&&a<=z)||(a>=Z&&a<=Z))
+PRINTF("WORD IS ALPHABIT");
+else
+printf("WORD IS NOT ALPHABT");
+}
