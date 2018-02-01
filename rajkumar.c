9bf7521ccda0ae2d46aa63ab1f193d6c5ff50e6d
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-02-01 15:38:17 +0530

    Create rajkumar.c

---
+++ rajkumar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char f='a'||'e'||'i'||'o'||'u'||'A'||'E'||'I'||'O'||'U';
+char g;
+printf("enter g letter");
+scanf("%c",&g);
+if(f==g)
+printf("letter is vowles");
+else
+printf("letter is not vowles");
+}
