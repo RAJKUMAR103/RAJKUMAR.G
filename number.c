0ea4bd1103ae029dd923179e9bf0b24259752019
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-02-09 16:55:29 +0530

    Create number.c

---
+++ number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i;
+    printf("enter a value");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        b=a*i;
+        printf("%d",b);
+        
+    }
+}
