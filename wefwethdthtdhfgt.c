9b470ad48084754cf5ae7680b131ed47efcdce04
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-02-13 15:53:56 +0530

    Create wefwethdthtdhfgt.c

---
+++ wefwethdthtdhfgt.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i,j;
+    printf("enter a,b values");
+    scanf("%d%d",&a,&b);
+    for(i=a;i<b;i++)
+    {
+        for(j=i;j<i;j++)
+    }
+    if(i==j)
+    printf("%d",j);
+    
+}
