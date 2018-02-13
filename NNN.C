ae586641f34cc2f230c2d1358503bb36af7db907
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-02-13 16:31:24 +0530

    Create NNN.C

---
+++ NNN.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+    int n,a[n],max=a[0],i;
+    printf("enter n values ");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    scanf("%d",&a[i]);
+    for(i=0;i<n;i++)
+     if(a[i]<max)
+         max=a[i];
+     printf("%d",max);
+}
+    
