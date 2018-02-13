2f50dc112842337bb30755e84eda3c6ecbb27449
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-02-13 15:57:43 +0530

    Create qqqq.c

---
+++ qqqq.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i,j;
+    printf("enter a,b values");
+    scanf("%d%d",&a,&b);
+    for(i=a;i<b;i++)
+    {
+        for(j=i;j<i;j++)
+    }
+    if(i%j==0
+    break;)
+    if(i==j)
+    printf("%d",j);
+    
+}
