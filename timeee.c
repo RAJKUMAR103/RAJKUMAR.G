6e57838cdca74e47efdd62fbcd6a4642873ea93e
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-02-27 16:14:03 +0530

    Create timeee.c

---
+++ timeee.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+ void main()
+ {
+     float a,b,c;
+     printf("enter a,b times with hours and minutes");
+     scanf("%f%f",&a,&b);
+     c=a-b;
+     printf("hours and minutes%f",c);
+     
+ }
