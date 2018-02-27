a1fa6532a5f082f9338490666b50104159718056
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-02-27 15:48:38 +0530

    Create sting.c

---
+++ sting.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    char a[3],i,b;
+    printf("enter three values");
+    for(i=0;i<3;i++)
+   
+    {
+        scanf("%d",&a);
+        
+    }
+     printf("enter the value");
+    scanf("%d",&b);
+    if(b==a[0]&&b==a[1]&&b==a[2])
+    printf("number is string");
+    else
+    printf("number is not string");
+}
