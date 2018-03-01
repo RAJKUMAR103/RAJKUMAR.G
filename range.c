b13eb746a2e7beab3cb4438ee99866f2e736fa9d
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-03-01 15:41:58 +0530

    Create range.c

---
+++ range.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i,c;
+    printf("enter  a ,b ,c values");
+    scanf("%d%d%d",&a,&b,&c);
+    if(c>=a&&c<=b)
+     printf("number is available between a,b");
+    else
+    printf("number is not available between a,b");
+    
+}
+
