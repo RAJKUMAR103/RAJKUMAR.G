b40e06e1a5e32ee067967dfa1bbe463908722c66
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-03-16 15:39:50 +0530

    Create count space.c

---
+++ count space.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[20],i,b=0;
+    printf("enter the letters:");
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==' ')
+        {
+            b++;
+        }
+    }
+    printf("%d",b);
+}
