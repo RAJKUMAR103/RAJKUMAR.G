a2c0436175bb8ebf9ebbf06733661dff7ce4c8c4
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-03-16 15:58:23 +0530

    Create count spl.c

---
+++ count spl.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    char a[20],i,b=0;
+    printf("enter the letters:");
+    gets(a);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if((a[i]>='0')&&(a[i]<='9')||(a[i]>='a')&&(a[i]<='z'))
+        {
+            continue;
+        }
+        else
+        {
+            b++;
+        }
+    }
+    printf("%d",b);
+}
