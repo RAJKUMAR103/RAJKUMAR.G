e41b3968ba1b9345fe66ce16d8ca0633de2a8fac
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-03-20 16:46:24 +0530

    Create multiply 13.c

---
+++ multiply 13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+   int a;
+   printf("enter a values:");
+   scanf("%d",&a);
+   if(a%13==0)
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+   }
+}
