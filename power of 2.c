c22a7a189c4e23c878bb3136a798ecb8cdb13616
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-03-20 16:18:41 +0530

    Create power of 2.c

---
+++ power of 2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int a,b;
+    printf("enter a values:");
+    scanf("%d",&a);
+    b=a*2;
+    printf("greater number of power 2 is:%d",b);
+}
