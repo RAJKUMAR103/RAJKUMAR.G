2d8b99f3bc83cce9745a5f8b8fee76805b80693a
RAJKUMAR103 <35728079+RAJKUMAR103@users.noreply.github.com>
2018-03-20 15:32:38 +0530

    Create guys.c

---
+++ guys.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int a;
+    printf("enter a values:");
+    scanf("%d",&a);
+    if(a%2==0)
+    printf("it is a prime no");
+    else
+    printf("it is not a prime");
+}
